6dfc4a285264c4b0cbd72b81d1f4f87a71f5bcf3
lx <33871533+ProgrammeAndroid@users.noreply.github.com>
2018-09-25 01:42:31 +0800

    部分实现预约详情页面

--- app/src/main/java/cn/ehanmy/hospital/mvp/ui/activity/UserAppointmentInfoActivity.java
+++ app/src/main/java/cn/ehanmy/hospital/mvp/ui/activity/UserAppointmentInfoActivity.java
@@ -4,14 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
+import com.jess.arms.http.imageloader.ImageLoader;
+import com.jess.arms.http.imageloader.glide.ImageConfigImpl;
 import com.jess.arms.utils.ArmsUtils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
 import cn.ehanmy.hospital.di.component.DaggerUserAppointmentInfoComponent;
 import cn.ehanmy.hospital.di.module.UserAppointmentInfoModule;
 import cn.ehanmy.hospital.mvp.contract.UserAppointmentInfoContract;
+import cn.ehanmy.hospital.mvp.model.entity.order.GoodsOrderBean;
+import cn.ehanmy.hospital.mvp.model.entity.order.OrderInfoBean;
+import cn.ehanmy.hospital.mvp.model.entity.order.OrderRecipientInfoBean;
+import cn.ehanmy.hospital.mvp.model.entity.user_appointment.OrderProjectDetailBean;
+import cn.ehanmy.hospital.mvp.model.entity.user_appointment.UserAppointmentGoodsBean;
 import cn.ehanmy.hospital.mvp.presenter.UserAppointmentInfoPresenter;
 
 import cn.ehanmy.hospital.R;
@@ -21,6 +37,54 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 
 public class UserAppointmentInfoActivity extends BaseActivity<UserAppointmentInfoPresenter> implements UserAppointmentInfoContract.View {
+
+
+    public static final String KEY_FOR_ORDER_ID = "key_for_order_id";
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    @Inject
+    ImageLoader mImageLoader;
+
+    @BindView(R.id.title_Layout)
+    View title;
+    @BindView(R.id.form_id)
+    TextView form_id;
+    @BindView(R.id.form_state)
+    TextView form_state;
+
+    @BindView(R.id.name)
+    TextView name;
+    @BindView(R.id.time)
+    TextView time;
+    @BindView(R.id.form_tel)
+    TextView form_tel;
+    @BindView(R.id.form_add)
+    TextView form_add;
+    @BindView(R.id.image)
+    ImageView image;
+    @BindView(R.id.project_name)
+    TextView project_name;
+    @BindView(R.id.order_time)
+    TextView order_time;
+
+
+    public void updateOrderInfo(OrderProjectDetailBean orderInfoBean){
+        UserAppointmentGoodsBean goodsOrderBean = orderInfoBean.getGoods();
+        mImageLoader.loadImage(this,
+                ImageConfigImpl
+                        .builder()
+                        .url(goodsOrderBean.getImage())
+                        .imageView(image)
+                        .build());
+
+        form_id.setText(orderInfoBean.getReservationId());
+        form_state.setText(orderInfoBean.getStatusDesc());
+        time.setText(orderInfoBean.getCreateDate());
+        name.setText(orderInfoBean.getMember().getMemberId());
+        form_tel.setText(orderInfoBean.getMember().getMobile());
+        form_add.setText("");
+        project_name.setText(goodsOrderBean.getName());
+        order_time.setText(orderInfoBean.getReservationTime());
+    }
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
