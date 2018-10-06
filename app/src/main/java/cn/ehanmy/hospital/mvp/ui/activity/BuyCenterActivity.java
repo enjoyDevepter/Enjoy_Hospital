a91d25fde0a85dde8b5d9fe94ed39672a7715190
lx <33871533+ProgrammeAndroid@users.noreply.github.com>
2018-10-07 00:46:52 +0800

    增加二维码识别页面，并增加通过验证码获取会员信息功能

--- app/src/main/java/cn/ehanmy/hospital/mvp/ui/activity/BuyCenterActivity.java
+++ app/src/main/java/cn/ehanmy/hospital/mvp/ui/activity/BuyCenterActivity.java
@@ -8,10 +8,15 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import cn.ehanmy.hospital.R;
@@ -22,6 +27,7 @@ import cn.ehanmy.hospital.mvp.model.entity.member_info.MemberBean;
 import cn.ehanmy.hospital.mvp.presenter.BuyCenterPresenter;
 import cn.ehanmy.hospital.util.CacheUtil;
 
+import static com.google.zxing.BarcodeFormat.QR_CODE;
 import static com.jess.arms.utils.Preconditions.checkNotNull;
 
 /**
@@ -44,6 +50,9 @@ public class BuyCenterActivity extends BaseActivity<BuyCenterPresenter> implemen
     @BindView(R.id.buy)
     View buy;
 
+    @BindView(R.id.qr_code)
+    View qr_code;  // 二维码
+
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
         DaggerBuyCenterComponent //如找不到该类,请编译一下项目
@@ -59,12 +68,46 @@ public class BuyCenterActivity extends BaseActivity<BuyCenterPresenter> implemen
         return R.layout.activity_buy_center; //如果你不需要框架帮你设置 setContentView(id) 需要自行设置,请返回 0
     }
 
+    // 启动扫描二维码界面
+    public void customScan(){
+        ArrayList<String> desiredBarcodeFormats = new ArrayList<String>();
+        desiredBarcodeFormats.add(QR_CODE.name());
+        new IntentIntegrator(this)
+                .setDesiredBarcodeFormats(desiredBarcodeFormats)
+                .setOrientationLocked(false)
+                .setCaptureActivity(SustomScanActivity.class) // 设置自定义的activity是CustomActivity
+                .initiateScan(); // 初始化扫描
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult != null) {
+            if(intentResult.getContents() == null) {
+                showMessage("扫描失败，请重试");
+            } else {
+                // 扫描二维码成功
+                // ScanResult 为 获取到的字符串
+                String ScanResult = intentResult.getContents();
+                search_key.setText(ScanResult);
+                mPresenter.requestHospitalInfo(ScanResult);
+            }
+        } else {
+            super.onActivityResult(requestCode,resultCode,data);
+        }
+    }
+
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
         new TitleUtil(title, this, "下单中心");
         clear_btn.setVisibility(View.GONE);
         buy.setOnClickListener(this);
         search_btn.setOnClickListener(this);
+        qr_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customScan();
+            }
+        });
     }
 
     @Override
@@ -101,7 +144,7 @@ public class BuyCenterActivity extends BaseActivity<BuyCenterPresenter> implemen
         String defaultStr = "会员编号错误，请重新查询！";
         if(codeIsRight){
             MemberBean memberBean = CacheUtil.getConstant(CacheUtil.CACHE_KEY_MEMBER);
-            defaultStr = memberBean.getNickName();
+            defaultStr = "会员编号正确，请继续下单\n"+memberBean.getNickName();
             if(!TextUtils.isEmpty(memberBean.getRealName())){
                 defaultStr += ("("+memberBean.getRealName()+")");
             }
