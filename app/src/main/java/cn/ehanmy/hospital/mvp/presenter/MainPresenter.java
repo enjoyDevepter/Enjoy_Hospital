3e1ab2e575ee039bc400b89f816a9ee7fdf8adb3
guomin <guomin@mapbar.com>
2018-11-07 10:58:25 +0800

    chg 请求参数、渠道号统一

--- app/src/main/java/cn/ehanmy/hospital/mvp/presenter/MainPresenter.java
+++ app/src/main/java/cn/ehanmy/hospital/mvp/presenter/MainPresenter.java
@@ -82,7 +82,7 @@ public class MainPresenter extends BasePresenter<MainContract.Model, MainContrac
 
     private void checkUpdateForApp() {
         UpdateRequest request = new UpdateRequest();
-        request.setType("4");
+        request.setType("1");
         mModel.checkUpdate(request)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
